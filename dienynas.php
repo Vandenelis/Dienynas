1dc501398da5ed3cbc9161f7651b959bcfc2cd23
Vandenelis <61286831+Vandenelis@users.noreply.github.com>
2020-02-22 18:21:55 +0200

    Sudėjau dienyno failus
    Kad viskas būt vienoje vietoje

---
+++ dienynas.php
@@ -0,0 +1,50 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+$saved = " ";
+$marksFilename = 'marks.txt';
+if (isset($_GET['submitted'])) {
+    $studentMark = $_GET['student']." ".$_GET['subject']." ".$_GET['mark']."\n";
+    file_put_contents($marksFilename, $studentMark, FILE_APPEND);
+    $saved = "Išsaugota";
+}
+$peopleFilename = 'people.txt';
+@$peopleFile = fopen(@$peopleFilename, "r");
+$studentListWarning = " ";
+if (!file_exists($peopleFilename) or !is_readable($peopleFilename)) {
+    $studentListWarning = (include 'studentListWarning.php');
+}
+$studentOptions = "";
+for ($line = fgets($peopleFile); !feof($peopleFile); $line = fgets($peopleFile)) {
+    $studentOptions .= "<option>{$line}</option>";
+}
+@$marksFile = fopen(@$marksFilename, "r");
+$studentMarksWarning = " ";
+if (!file_exists($marksFilename) or !is_readable($marksFilename)) {
+    $studentMarksWarning = (include 'studentMarksWarning.php');
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang = "lt">
+    <head>
+        <title>Naujo pažymio įvedimas</title>
+    </head>
+    <body>
+        <?php $studentMarksWarning.$studentListWarning?> 
+        <p><?= $saved?></p>
+        <h2>Įrašykite pažymį</h2>
+        <form action = '' method = 'get'>
+            <div>Mokinys:</div>
+            <div>
+                <select name = 'student'>
+                    <?= $studentOptions ?>
+                </select>
+            </div>
+            <div>Dalykas:</div>
+            <div><input type = 'text' name = 'subject' value = "Matematika"></div>
+            <div>Pažymys:</div>
+            <div><input type = 'text' name = 'mark' value = "7"></div>
+            <div><input type = 'submit' name = 'submitted' value = "Išsaugoti"></div>
+        </form>
+    </body>
+</html>
