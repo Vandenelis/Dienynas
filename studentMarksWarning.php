1de2c806ed10da88817098f07647b0a74cfe23a8
Vandenelis <ievuze12@gmail.com>
2020-02-27 21:44:51 +0200

    Commiting missing files

---
+++ studentMarksWarning.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang = "lt">
+    <head>
+        <title>Naujo pažymio įvedimas</title>
+    </head>
+    <body>
+        <div>Nepavyksta atidaryti failo su mokinių pažymiais!</div>
+    </body>
+</html>
+    <?php
+    exit();
+    ?>
