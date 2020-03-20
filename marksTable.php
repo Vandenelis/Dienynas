1218b2c147c02327a921e4ad075cb8154f81493a
Vandenelis <ievuze12@gmail.com>
2020-03-20 20:26:38 +0200

    Attempt to create marks table

---
+++ marksTable.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+</head>
+<body>
+    <table border = 1>
+            <tr>
+                <th>Eil. nr.</th>
+                <th>Vardas</th>
+                <th>Pavardė</th>
+                <th>Dalykas</th>
+                <th>Pažymys</th>
+            </tr>
+            
