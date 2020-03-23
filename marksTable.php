2c881dc9dae3b492a90d51a0c7c5b376119d7fe9
Vandenelis <ievuze12@gmail.com>
2020-03-23 11:34:12 +0200

    marksTable error checking

--- marksTable.php
+++ marksTable.php
@@ -1,9 +1,21 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+$marksFilename = 'marks.txt';
+@$marksFile = fopen(@$marksFilename, "r");
+if (!file_exists($marksFilename) or !is_readable($marksFilename)) {
+    $errorMessage = "Nepavyksta atidaryti failo su mokinių pažymiais!";
+    include 'errorTemplate.php';
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="lt">
-<head>
-</head>
-<body>
-    <table border = 1>
+    <head>
+        <title>Pažymių lentelė</title>
+    </head>
+    <body>
+        <table border = 1>
             <tr>
                 <th>Eil. nr.</th>
                 <th>Vardas</th>
@@ -12,18 +24,11 @@
                 <th>Pažymys</th>
             </tr>
 <?php
-$marksFilename = 'marks.txt';
-$marksFile = fopen($marksFilename, "r");
-if (!file_exists($marksFilename) or !is_writable($marksFilename)) {
-    $errorMessage = "Nepavyksta atidaryti failo su mokinių pažymiais!";
-    include 'errorTemplate.php';
-    exit();
-}
 for ($line = fgets($marksFile), $i = 1; !feof($marksFile); $line = fgets($marksFile), $i++) {
     $studentDataChunk = explode (" ", $line);
     echo "<tr><td>".$i."</td><td>".$studentDataChunk[0]."</td><td>".$studentDataChunk[1]."</td><td>".$studentDataChunk[2]."</td><td>".$studentDataChunk[3]."</td></tr>";
 }
 ?>
-    </table>
-</body>
+        </table>
+    </body>
 </html>
